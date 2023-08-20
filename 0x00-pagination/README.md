a71dbba92c5f4676ef4a4a516919cc39168e0f01
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-20 17:30:56 +0100

    Hyper

--- 0x00-pagination/README.md
+++ 0x00-pagination/README.md
@@ -1,1 +1,8 @@
 ## 0x00-pagination
+
+## Resources
+
+- [REST API Design: Pagination](https://intranet.alxswe.com/rltoken/7Kdzi9CH1LdSfNQ4RaJUQw)
+
+- [HATEOAS](https://intranet.alxswe.com/rltoken/tfzcEbTSdMYSYxsspJH_oA)
+
---
+++ 0x00-pagination/paginate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 34, 9, 87, 69, 34, 65, 78, 89, 43, 34]
+print(len(dataset))
+
+# Parameters
+page = 3
+page_size = 6  # Number of items per page
+
+# Calculate the starting and ending indices for the requested page
+start_index = (page - 1) * page_size
+end_index = start_index + page_size
+
+# Slice the dataset to get the desired page
+paged_data = dataset[start_index:end_index]
+
+# Print the paged data
+print(paged_data)
+
+# Paginating list of names
+names = ['Al-Areef', 'Ayompo', 'Adegbite', 'Aa`isha', 'Al-Ameen', 'Biggie', 'Brr tob', 'Nuccas']
+print(len(names))
+page = 1
+page_size = 5
+start = (page -1) * page_size
+end = start + page_size
+data = names[start:end]
+print(data)
