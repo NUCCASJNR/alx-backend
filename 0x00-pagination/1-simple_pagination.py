6fce24fb5b959f21dc4b620f64e6e70091768ee6
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-21 10:15:42 +0100

    typeof page and page size

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -43,7 +43,8 @@ class Server:
         """
         Get page
         """
-        assert isinstance(page, int) and isinstance(page_size, int)
+        assert (type(page) == int)
+        assert (type(page_size) == int)
         assert page > 0 and page_size > 0
         data = self.dataset()
         start, end = index_range(page, page_size)
