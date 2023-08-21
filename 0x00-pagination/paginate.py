82f449fc8450198a526fc3a8478e92bd94d9a4c2
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-21 02:43:55 +0100

    pagination from a CSV file

--- 0x00-pagination/paginate.py
+++ 0x00-pagination/paginate.py
@@ -2,7 +2,7 @@
 
 dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 34, 9, 87, 69, 34, 65, 78, 89, 43, 34]
 print(len(dataset))
-
+print(dataset[18])
 # Parameters
 page = 3
 page_size = 6  # Number of items per page
@@ -10,7 +10,8 @@ page_size = 6  # Number of items per page
 # Calculate the starting and ending indices for the requested page
 start_index = (page - 1) * page_size
 end_index = start_index + page_size
-
+print(start_index)
+print(end_index)
 # Slice the dataset to get the desired page
 paged_data = dataset[start_index:end_index]
 
