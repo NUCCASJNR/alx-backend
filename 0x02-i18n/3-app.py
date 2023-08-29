1e507a0e69c7e4d74ecc161af9c4b2531f1101cd
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-29 22:36:12 +0100

    Translate

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,19 +3,25 @@
 """Contains a flask app and a babel class"""
 
 from flask import Flask, render_template
-from flask_babel import Babel, gettext
+from flask_babel import Babel, gettext, request
 
 
 class Config(object):
     """Config class for babel"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Translates to the best match language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
