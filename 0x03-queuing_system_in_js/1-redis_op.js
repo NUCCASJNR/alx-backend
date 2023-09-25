ed4a00c18611e7595133a4e159a2fdda56a34a79
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-25 14:47:09 +0100

    Update task 1

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -3,25 +3,19 @@ const redisPort = 6379;
 const redisHost = 'localhost';
 const client = redis.createClient(redisPort, redisHost);
 
-// Establish a connection on the redis server
+// Establish a connection to the Redis server
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-// Handle any connection error
+// Handle any connection errors
 client.on('error', (err) => {
   console.error(`Redis client not connected to the server: ${err}`);
 });
 
 const setNewSchool = (schoolName, value) => {
-  client.set(schoolName, value, (err, response) => {
-    if (err) {
-      console.error(`Redis Error: ${err}`);
-    } else {
-      console.log(`Reply: ${response}`);
-    }
-  });
+  client.set(schoolName, value, redis.print);
 };
 
 const displaySchoolValue = (schoolName) => {
@@ -34,7 +28,10 @@ const displaySchoolValue = (schoolName) => {
   });
 };
 
+// Call the functions to interact with Redis
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
 displaySchoolValue('HolbertonSanFrancisco');
+
+// Quit the Redis client when done
 client.quit();
